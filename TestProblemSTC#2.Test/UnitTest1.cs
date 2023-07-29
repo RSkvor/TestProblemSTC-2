6630efcc3b146b35c95695db69150538c71ba7a1
Roman Skvortsov <goodlock01@yandex.ru>
2023-07-30 00:00:47 +0500

    #1. Перенос файлов для добавления проекта с тестами

---
+++ TestProblemSTC#2.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace TestProblemSTC_2.Test
+{
+    public class AlgorithmTest
+    {
+        [SetUp]
+        public void Init()
+        {
+
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
